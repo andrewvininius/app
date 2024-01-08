f7d40e86c08d6577e8c1973804d553e65f746b12
Andrew Vinicius <74573141+andrewvininius@users.noreply.github.com>
2024-01-08 19:23:18 -0300

    Add files via upload

--- Pegar os dados/app3.py
+++ Pegar os dados/app3.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import json
 
 app = Flask(__name__)
 
@@ -11,10 +12,17 @@ def webhook():
     if request.method == 'POST':
         data = request.json
         print(data)
+
+        # Salvar em um arquivo (anexar a um arquivo existente ou criar um novo).
+        with open('depositos.json', 'a') as file:
+            file.write(json.dumps(data) + '\n')
+
         return 'Webhook recebido', 200
     else:
         return 'Webhook aceita apenas método POST', 200
 
-
 if __name__ == '__main__':
     app.run(debug=True)
+
+#$body = @{chave="valor"} | ConvertTo-Json
+#Invoke-WebRequest -Uri http://127.0.0.1:5000/webhook -Method POST -ContentType "application/json" -Body $body -UseBasicParsing
